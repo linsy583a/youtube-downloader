9d93192f7474c97c809a576752d568c7e861c239
linsy583a <84334685+linsy583a@users.noreply.github.com>
2021-06-10 00:34:52 -0400

    Update list-edit.php

--- public/list-edit.php
+++ public/list-edit.php
@@ -36,9 +36,11 @@ $text = trim(explode('</sele',$text)[0]);
 ?>
 
 <form action="" accept-charset="utf-8" method="post">
+      <input type="submit" value="update">
 <textarea id="list" name="list" rows="20" cols="80">
 <?php echo $text ?>
   </textarea>
+    
 </form>
 
 
