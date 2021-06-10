92e11ff4722d4a4767a0fcb5080c348199eeca2a
linsy583a <84334685+linsy583a@users.noreply.github.com>
2021-06-10 12:08:08 -0400

    fetch from alltb-tpl.herokuapp

---
+++ public/fetch1.php
@@ -0,0 +1,37 @@
+<?php
+function getTrueURL($url)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_NOBODY, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_exec($ch);
+    $data = curl_getinfo($ch);
+    return $data["url"];
+}
+
+$videoLink = trim($_REQUEST['url']);
+$aiw = $_REQUEST['aiw'];
+$videoLink = "https://alltb-tpl.herokuapp.com/download?url=".$videoLink;
+
+if(!empty($_REQUEST['sd'])) { $videoLink .= "&format=18"; }
+$turl = getTrueURL($videoLink);
+
+if(!empty($url) && !empty($aiw)) {
+        echo '<p><a href="'.$turl.'">'.$turl.'</a>';
+        echo '<p><a href="stream.php?url='.urlencode($turl).'">stream</a>';
+} elseif (!empty($url)) {
+        echo $turl;     
+}
+else {
+echo '<a href="/"></a> | ';
+echo '<a href="ytc.php">YTC</a>';
+}
+?>
+<html>
+<script>
+    if(!empty($url) && !empty($aiw)) {
+echo 'window.AppInventor.setWebViewString("'.$turl.'");';
+    }
+</script>
+</html>
