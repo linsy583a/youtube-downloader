88bcd73f1550fb9609ad3e308a6b098b5e602968
linsy583a <84334685+linsy583a@users.noreply.github.com>
2021-06-10 01:22:17 -0400

    Update this.php

--- public/this.php
+++ public/this.php
@@ -1,8 +1,11 @@
 <?php
 $url = trim($_REQUEST['url']);
-    if(!empty($url)) {
+$aiw = $_REQUEST['aiw'];
+    if(!empty($url) && !empty($aiw)) {
         echo '<p><a href="'.$url.'">'.$url.'</a><br>';  
         echo '<p><a href="stream.php?url='.urlencode($url).'">stream</a><br>';
+    } elseif (!empty($url)) {
+        echo $url;   
     }
     else {
         echo '<a href="/">Back</a>';
@@ -10,7 +13,7 @@ $url = trim($_REQUEST['url']);
 ?>
 <script>
 <?php
-    if(!empty($url)) {
+    if(!empty($url) && !empty($aiw)) {
 echo 'window.AppInventor.setWebViewString("'.urldecode($url).'");';
     }
 ?>
