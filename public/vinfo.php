027cb64f4a36c6c438d2005ca9c33b3d34dd03be
linsy583a <84334685+linsy583a@users.noreply.github.com>
2021-06-09 23:02:28 -0400

    Update vinfo.php

--- public/vinfo.php
+++ public/vinfo.php
@@ -3,8 +3,6 @@
 require('../vendor/autoload.php');
 
 $url = isset($_GET['url']) ? $_GET['url'] : null;
-$hd = isset($_GET['hd']) ? $_GET['hd'] : null;
-$allFormats = isset($_GET['all']) ? $_GET['all'] : null;
 $detail = isset($_GET['detail']) ? $_GET['detail'] : null;
 function send_json($data)
 {
@@ -23,35 +21,23 @@ $youtube = new \YouTube\YouTubeDownloader();
 
 try {
     $links = $youtube->getDownloadLinks($url);
-    if (!$allFormats) {
-    $best = (!$hd) ? $links->getFirstCombinedFormat() : $links->getFirstCombinedFormat2();
+    $best = $links->getAllCombinedFormat();
+    //$best = (!$hd) ? $links->getFirstCombinedFormat() : $links->getFirstCombinedFormat2();
     
     if ($best) {
-        send_json([
-            'links' => [$best->url]
-        ]);
-        
-    } else {
-        send_json(['error' => 'No links found']);
-    } } else {
-        $best = $links->getAllCombinedFormat();
-        if ($best) {
             if (!$detail){
                 $text = [];
-            foreach ($best as $item) {
-                $text[] = $item->url;
-             }
-            send_json([
-            'links' => $text
-        ]);
+                foreach ($best as $item) {
+                    $text[] = $item->url;
+                }
+                send_json([
+                    'links' => $text
+                ]);
             } else {
                 send_json($best);
             }
         } else {
-        send_json(['error' => 'No links found']);
-    } 
-        
-        
+        send_json(['error' => 'No links found']);      
     }
 
 } catch (\YouTube\Exception\YouTubeException $e) {
